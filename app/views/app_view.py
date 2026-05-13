b3097042408f1fc92fd129f94033ab24a3df606e
vini1602648 <vinibiscoito46@gmail.com>
2026-05-12 21:24:31 -0300

    Update app_view.py

--- app/views/app_view.py
+++ app/views/app_view.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import os
 
-API_URL = "https://app-crud-gtob.onrender.com"
+API_URL = "https://novo-instala-o-1.onrender.com"
 
 st.set_page_config(page_title="Biblioteca Digital", layout="wide")
 
